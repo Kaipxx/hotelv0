b06194bc3bcb3c180000fc1cf68dba050bb7d092
Dmitriy <kaiplolz123@gmail.com>
2025-03-19 16:40:44 +0300

    add models and sql

--- app/database.py
+++ app/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+#from config import settings
+from app.config import settings
 
-
-#где находится база данных
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+#DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 # создаём движок. для передачи url в алхимию
-engine = create_async_engine(DATABASE_URL, future=True)
+engine = create_async_engine(settings.DATABASE_URL, future=True)
 #создаём сессии(транзакций)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
